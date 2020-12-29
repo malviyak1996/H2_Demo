08ac0762576484f261e9be2dab062b3e30960bf7
kunal malviya <kunalmalviya1996@gmail.com>
2020-12-29 23:00:03 +0530

    renamed table name

--- src/main/java/com/howtodoinjava/demo/model/ActivationNotificationEntity.java
+++ src/main/java/com/howtodoinjava/demo/model/ActivationNotificationEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="TBL_EMPLOYEES")
+@Table(name="ACTIVATION_NOTIFICATION ")
 public class ActivationNotificationEntity {
 
 	@Id
